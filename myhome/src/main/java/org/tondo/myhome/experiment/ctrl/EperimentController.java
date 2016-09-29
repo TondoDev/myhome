cb967ae76111366b295242964d2afff080d277a3
TondoDev <tondodeveloper@gmail.com>
2016-09-29 22:12:46 +0200

    controller for showing coocings tree

--- myhome/src/main/java/org/tondo/myhome/experiment/ctrl/EperimentController.java
+++ myhome/src/main/java/org/tondo/myhome/experiment/ctrl/EperimentController.java
@@ -1,13 +1,17 @@
 package org.tondo.myhome.experiment.ctrl;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.tondo.myhome.experiment.domain.Cooking;
+import org.tondo.myhome.experiment.domain.CookingRepository;
+import org.tondo.myhome.experiment.domain.Eating;
+import org.tondo.myhome.experiment.domain.EatingsRepository;
 import org.tondo.myhome.experiment.domain.Food;
 import org.tondo.myhome.experiment.domain.FoodRepository;
 
@@ -17,11 +21,34 @@ public class EperimentController {
 	@Autowired
 	private FoodRepository foodRepository;
 	
+	@Autowired
+	private CookingRepository cookingRepository;
+	
+	@Autowired
+	private EatingsRepository eatingRepository;
+	
 	@RequestMapping("/foodall/")
 	public List<Food> something() {
 		return foodRepository.findAll();
 	}
-
+	
+	@RequestMapping("/cookings/")
+	public List<Cooking> cookings() {
+		 List<Cooking> list  = cookingRepository.findAll();
+		 if (list != null && !list.isEmpty()) {
+			 Cooking cook = list.get(0);
+			 Collection<Eating> eatList = (Collection<Eating>) (cook.getEatings() == null ? new ArrayList<>() : new ArrayList<>(cook.getEatings()));
+			 Eating e = new Eating();
+			 e.setMealDate(new Date());
+			 e.setMealType("vecera");
+			 eatList.add(e);
+			 cook.setEatings(eatList);
+			 eatingRepository.save(e);
+			 cookingRepository.save(cook);
+		 }
+		 
+		 return list;
+	}
 
 	@RequestMapping("/food/")
 	public List<Food> getFoods() {
