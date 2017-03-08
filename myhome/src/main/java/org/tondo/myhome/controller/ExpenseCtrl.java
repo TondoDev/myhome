f29a6576ee755ba12382f144bdeb41c2a355a0b1
TondoDev <tondodeveloper@gmail.com>
2017-03-08 22:32:21 +0100

    incorporated thymeleaf into project
    with sample template

--- myhome/src/main/java/org/tondo/myhome/controller/ExpenseCtrl.java
+++ myhome/src/main/java/org/tondo/myhome/controller/ExpenseCtrl.java
@@ -1,23 +1,23 @@
 package org.tondo.myhome.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.tondo.myhome.domain.Expense;
 import org.tondo.myhome.service.ExpenseSvc;
 
 @Controller
-@RequestMapping("/exepnse")
+@RequestMapping("/expense")
 public class ExpenseCtrl {
 	
 	@Autowired
 	private ExpenseSvc expenseService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public List<Expense> findAll() {
-		return expenseService.getExpenses();
+	public String findAll(Model mode) {
+		String note = expenseService.getExpenses().get(0).getNote();
+		mode.addAttribute("name", note);
+		return "index";
 	}
 }
