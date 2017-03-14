d1a05cb230a6b2b19456054c0d9ee9813993ac3c
Tondo at work <tondo@work.com>
2017-03-14 08:33:04 +0100

    displayed table for expenses list

--- myhome/src/main/java/org/tondo/myhome/controller/ExpenseCtrl.java
+++ myhome/src/main/java/org/tondo/myhome/controller/ExpenseCtrl.java
@@ -1,10 +1,13 @@
 package org.tondo.myhome.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.tondo.myhome.domain.Expense;
 import org.tondo.myhome.service.ExpenseSvc;
 
 @Controller
@@ -15,9 +18,11 @@ public class ExpenseCtrl {
 	private ExpenseSvc expenseService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String findAll(Model mode) {
-		String note = expenseService.getExpenses().get(0).getNote();
-		mode.addAttribute("name", note);
+	public String findAll(Model model) {
+		List<Expense> expenses = expenseService.getExpenses();
+		String note = expenses.get(0).getNote();
+		model.addAttribute("expenses", expenses);
+		model.addAttribute("name", note);
 		return "index";
 	}
 }
