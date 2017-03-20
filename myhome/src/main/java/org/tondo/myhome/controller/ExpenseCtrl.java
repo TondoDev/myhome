0de2863f51ebb2d7ddffaf06572a8b271138e313
TondoDev <tondodeveloper@gmail.com>
2017-03-20 22:48:58 +0100

    first POST form attempt

--- myhome/src/main/java/org/tondo/myhome/controller/ExpenseCtrl.java
+++ myhome/src/main/java/org/tondo/myhome/controller/ExpenseCtrl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.tondo.myhome.domain.Expense;
@@ -23,6 +24,14 @@ public class ExpenseCtrl {
 		String note = expenses.get(0).getNote();
 		model.addAttribute("expenses", expenses);
 		model.addAttribute("name", note);
+		// for store form input
+		model.addAttribute("expense", new Expense());
+		return "index";
+	}
+	
+	@RequestMapping
+	public String addExpense(@ModelAttribute("expense") Expense expense) {
+		System.out.println(expense.getAmount());
 		return "index";
 	}
 }
