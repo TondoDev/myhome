65f5de0361996bd4380472bbd03f1940af679304
TondoDev <tondodeveloper@gmail.com>
2017-10-22 00:17:20 +0200

    removed redundat entity for enum

--- myhome/src/main/java/org/tondo/myhome/enumsdom/EnumRepository.java
+++
@@ -1,8 +0,0 @@
-package org.tondo.myhome.enumsdom;
-//package org.tondo.myhome.enumsvc;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface EnumRepository extends JpaRepository<EnumStruct, Integer> {
-//	
-//}
