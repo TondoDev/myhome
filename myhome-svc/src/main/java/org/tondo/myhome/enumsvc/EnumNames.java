8695364df03812228465edb62f07cc0e9072cf6e
TondoDev <tondodeveloper@gmail.com>
2017-12-04 21:48:44 +0100

    created empty svc and thymeleaf projects

---
+++ myhome-svc/src/main/java/org/tondo/myhome/enumsvc/EnumNames.java
@@ -0,0 +1,10 @@
+package org.tondo.myhome.enumsvc;
+
+public final class EnumNames {
+
+	private EnumNames() {
+		// prevents instantiation
+	}
+	
+	public static final String EXPENSES = "EXPENSES";
+}
